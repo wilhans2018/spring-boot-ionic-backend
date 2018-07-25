3cf764fa58ad4f6cfac8e10b5667b6801d2edb53
Wilhans souza <wilhans1984@hotmail.com>
2018-07-25 18:08:00 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/wilhans/cursomc/resources/exceptions/FieldMessage.java
@@ -0,0 +1,37 @@
+package com.wilhans.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String fildName;
+	private String message;
+
+	public FieldMessage() {
+
+	}
+
+	public FieldMessage(String fildName, String message) {
+		super();
+		this.setFildName(fildName);
+		this.setMessage(message);
+	}
+
+	public String getFildName() {
+		return fildName;
+	}
+
+	public void setFildName(String fildName) {
+		this.fildName = fildName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
