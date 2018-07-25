48268f7a5145a1669ad46d0787894c3de029b39b
Wilhans souza <wilhans1984@hotmail.com>
2018-07-25 17:09:00 -0300

    Validação sintática com Bean Validation Categorias

--- src/main/java/com/wilhans/cursomc/services/CategoriaService.java
+++ src/main/java/com/wilhans/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wilhans.cursomc.domain.Categoria;
+import com.wilhans.cursomc.dto.CategoriaDTO;
 import com.wilhans.cursomc.repositories.CategoriaRepository;
 import com.wilhans.cursomc.services.exceptions.DataIntegreityException;
 import com.wilhans.cursomc.services.exceptions.ObjNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
