08ac2b67241096347e63630d5b8188b3f1e80ae7
Wilhans souza <wilhans1984@hotmail.com>
2018-07-27 10:02:06 -0300

    Email nao repetido na insercaod de cliente

--- src/main/java/com/wilhans/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/wilhans/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.wilhans.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.wilhans.cursomc.domain.Cliente;;;;
+import com.wilhans.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
